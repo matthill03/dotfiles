ccc608cc67416de56b46d986c68bc2068c76704d
Matthew Hill <contact@matthew-hill.net>
2024-10-15 22:33:56 +0100

    Editing hyprland, wayland and nvim configs

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -31,6 +31,9 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Easy way to exit insert mode
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Easy way to exit insert mode" })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
