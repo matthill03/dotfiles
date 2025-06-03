d6c2c2a013f33247d2ddb714d2bbf6961b085022
Matthew Hill <contact@matthew-hill.net>
2025-06-03 22:40:12 +0100

    Adding new config stuff

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -2,6 +2,8 @@ vim.keymap.set("i", "jk", "<ESC>", { desc = "Easy way to exit insert mode" })
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
+vim.keymap.set("n", "\\", "<cmd>Oil<CR>")
+
 vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
