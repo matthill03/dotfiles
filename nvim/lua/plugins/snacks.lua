5ab28625f64b7a9ad7397f6e967352c7a0f78e5d
Matthew Hill <contact@matthew-hill.net>
2025-06-10 23:36:24 +0100

    Macos changes

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -3,109 +3,13 @@ return {
     priority = 1000,
     lazy = false,
     opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = {
-        enabled = true,
-        preset = {
-        keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":Telescope find_files" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":Telescope live_grep" },
-            { icon = " ", key = "r", desc = "Recent Files", action = ":Telescope oldfiles" },
-            { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-            { icon = " ", key = "s", desc = "Restore Session", section = "session" },
-            { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
-            },
-        },
-        sections = {
-            { section = "header" },
-            {
-              pane = 2,
-              section = "terminal",
-              cmd = "colorscript -e square",
-              height = 5,
-              padding = 1,
-            },
-            { section = "keys", gap = 1, padding = 1 },
-            {
-              pane = 2,
-              icon = " ",
-              desc = "Browse Repo",
-              padding = 1,
-              key = "b",
-              action = function()
-                  Snacks.gitbrowse()
-              end,
-            },
-            function()
-              local in_git = Snacks.git.get_root() ~= nil
-              local cmds = {
-                {
-                    title = "Repositories",
-                    cmd = "gh repo list",
-                    action = function()
-                        vim.fn.jobstart("gh repo view --web", { detach = true })
-                    end,
-                    key = "n",
-                    icon = " ",
-                    height = 5,
-                    enabled = true,
-                },
-                {
-                    title = "Open Issues",
-                    cmd = "gh issue list -L 3",
-                    key = "i",
-                    action = function()
-                        vim.fn.jobstart("gh issue list --web", { detach = true })
-                    end,
-                    icon = " ",
-                    height = 7,
-                },
-                {
-                    icon = " ",
-                    title = "Open PRs",
-                    cmd = "gh pr list -L 3",
-                    key = "P",
-                    action = function()
-                        vim.fn.jobstart("gh pr list --web", { detach = true })
-                    end,
-                    height = 7,
-                },
-                {
-                    icon = " ",
-                    title = "Git Status",
-                    cmd = "git --no-pager diff --stat -B -M -C",
-                    height = 10,
-                },
-            }
-                return vim.tbl_map(function(cmd)
-                    return vim.tbl_extend("force", {
-                      pane = 2,
-                      section = "terminal",
-                      enabled = in_git,
-                      padding = 1,
-                      ttl = 5 * 60,
-                      indent = 3,
-                    }, cmd)
-                end, cmds)
-            end,
-            { section = "startup" },
-        },
-    },
-    explorer = { enabled = true },
     indent = { enabled = true },
-    input = { enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
     scope = { enabled = true },
     scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
     },
     keys = {
         -- git
